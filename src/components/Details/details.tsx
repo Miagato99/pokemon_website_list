fa702f8a96bc7b06fe2509ea883b532db1527218
Miagato99 <90808722+Miagato99@users.noreply.github.com>
2024-05-20 12:39:25 +0800

    Changes in details

--- src/components/Details/details.tsx
+++ src/components/Details/details.tsx
@@ -1,61 +1,70 @@
 import { FC } from "react";
 import Image from "next/image";
 import Button from "../Button/button";
-import Link from "next/link"
+import Link from "next/link";
 import { DetailsProps } from "./details-types";
-import "../../styles/details.scss"
+import "../../styles/details.scss";
 
 const Details: FC<DetailsProps> = ({ pokemon }) => {
   return (
-    <div className="detailsContainer">
-      <Image
-        src={pokemon.sprites.front_default}
-        alt={pokemon.name}
-        width={250}
-        height={250}
-      />
-      <h2 className="name">{pokemon.name}</h2>
-      <div className="info-section">
-        <h3>Types:</h3>
-        <ul>
-          {pokemon.types.map((type) => (
-            <li key={type.slot}>{type.type.name}</li>
-          ))}
-        </ul>
+    <div className="details-container">
+      <section className="image-section">
+        <div className="image-wrapper">
+          <Image
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name}
+            width={250}
+            height={250}
+            className="pokemon-image"
+          />
+        </div>
+        <h2 className="name">{pokemon.name}</h2>
+      </section>
+
+      <section className="info-section">
+        <div className="info-item">
+          <h3>Types:</h3>
+          <ul className="type-list">
+            {pokemon.types.map((type) => (
+              <li
+                key={type.slot}
+                className={`type ${type.type.name.toLowerCase()}`}
+              >
+                {type.type.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="info-item">
+          <h3>Abilities:</h3>
+          <ul>
+            {pokemon.abilities.map((ability) => (
+              <li key={ability.slot}>{ability.ability.name}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="info-item">
+          <h3>Stats:</h3>
+          <table className="stats-table">
+            <tbody>
+              {pokemon.stats.map((stat) => (
+                <tr key={stat.stat.name}>
+                  <td>{stat.stat.name}</td>
+                  <td>{stat.base_stat}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+      <div className="button-container">
+        <Link href={`/pokemon`}>
+          <Button btnText="Go Back" />
+        </Link>
       </div>
-      <div className="info-section">
-        <h3>Abilities:</h3>
-        <ul>
-          {pokemon.abilities.map((ability) => (
-            <li key={ability.slot}>{ability.ability.name}</li>
-          ))}
-        </ul>
-      </div>
-      <div className="info-section">
-        <h3>Base Experience:</h3>
-        <p>{pokemon.baseExperience}</p>
-      </div>
-      <div className="info-section">
-        <h3>Height:</h3>
-        <p>{pokemon.height}</p>
-      </div>
-      <div className="infoSection">
-        <h3>Weight:</h3>
-        <p>{pokemon.weight}</p>
-      </div>
-      <div className="info-section">
-        <h3>Stats:</h3>
-        <ul>
-          {pokemon.stats.map((stat) => (
-            <li key={stat.stat.name}>
-              {stat.stat.name}: {stat.base_stat}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <Link href={`/`}>
-        <Button btnText="Go Back" />
-      </Link>
     </div>
   );
 };
